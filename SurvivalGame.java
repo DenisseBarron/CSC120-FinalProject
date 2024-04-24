bf56fc17c302406be3c3f4ca269efcc5401bf196
Denisse Barron <abarronsalazar@smith.edu>
2024-04-24 17:51:34 -0400

    Denisse

--- SurvivalGame.java
+++ SurvivalGame.java
@@ -2,17 +2,27 @@ import java.util.Scanner;
 
 public class SurvivalGame {
     private static Scanner scanner = new Scanner(System.in);
-    public static int itemsCollected = 0;
+    private static int itemsCollected = 0;
+    private static int health = 10;
+    private static String userName;
+
 
     public static void main(String[] args){
          //Introduction to the game
          System.out.println("Day 1");
          System.out.println("You are left behind by your hinking group in a hunted campsite");
-         System.out.println("You have to decide what to do next in order to survive");
          System.out.println("You need to collect resources to survuve, but you can only choose three.");
 
+         //Ask User for name
+         System.out.println("What's your name?");
+         userName = scanner.nextLine();
+         System.out.println("Hello " + userName + "! Let's start your survival journey!");
+
          //Main loop to choose locations until all items are collected
-         while (itemsCollected < 3) {
+         while (itemsCollected < 3 && health > 0) {
+            //Display user's status
+            displayUserDetails();
+
             //User choose a location
             System.out.println("Choose a place: (1) Kitchen, (2) Woods, (3) Backstage, (4) Lake, (5) Firepit, (6) Tent, (7) Showers, (8) Lockers");
             int choice = scanner.nextInt();
@@ -49,6 +59,15 @@ public class SurvivalGame {
             }
          }
          //Display message when all items are collected
-         System.out.println("Congradulations! You have collected all necessary items to survive.");
+         if (itemsCollected >= 3) {
+            System.out.println("Congradulations! You have collected all necessary items to survive.");
+         } 
+     }
+     private static void displayUserDetails(){
+        System.out.println("---------------------");
+        System.out.println(userName + "'s Status:");
+        System.out.println("Health: " + health);
+        System.out.println("itemsCollected: " + itemsCollected);
+        System.out.println("---------------------");
      }
 }
